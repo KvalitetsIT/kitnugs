6f3e7aeff95e8bb3b4cceb569422c7355bec3feb
Jonas Pedersen <jpe@kvalitetsit.dk>
2023-10-10 07:47:17 +0200

    Correct build of documentation image (#11)

--- Documentation/docker/getOldDoc.sh
+++ Documentation/docker/getOldDoc.sh
@@ -4,5 +4,5 @@ IMAGE=$1
 
 if docker pull $IMAGE:latest; then
     echo "Copy from old documentation image."
-    docker cp $(docker create $IMAGE:latest):/usr/share/nginx/html Documentation/old
+    docker cp $(docker create $IMAGE:latest):/usr/share/nginx/html Documentation/target
 fi
